9bc492648fc7277237a83f44308a9b81c30c5a69
g00291461 <g00291461@gmit.ie>
2018-03-05 15:23:23 +0000

    added locations as list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RestProvider } from '../../providers/rest/rest';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  buildings: any;
 
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
+    this.getBuildings();
+  }
+
+  getBuildings() {
+    this.restProvider.getBuildings()
+    .then(data => {
+      this.buildings = data;
+      console.log(this.buildings);
+    });
   }
 
 }
---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  apiUrl = 'https://my-json-server.typicode.com/conormc93/db';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+  getBuildings() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/buildings').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  
+}
